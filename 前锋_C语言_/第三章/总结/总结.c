d4f1bd32a9ccbb542feda12ca6a7c9e784d20154
CLAY <48152371+mineCLAY@users.noreply.github.com>
2019-05-10 20:24:43 +0800

    Create 总结.c

---
+++ 前锋_C语言_/第三章/总结/总结.c
@@ -0,0 +1,12 @@
+C 声明              32 位机器              64 位机器
+======             ==========            ==========
+
+char                   1                     1
+short int              2                     2
+int                    4                     4
+long int               4                     8
+long long int          8                     8
+char *                 4                     8
+float                  4                     4
+double                 8                     8
+long double            12                    16
