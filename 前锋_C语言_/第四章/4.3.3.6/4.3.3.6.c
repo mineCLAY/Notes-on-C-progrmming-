185b9fb7f0bf09aa7f8a9fd2f01651cdc8657fe4
CLAY <48152371+mineCLAY@users.noreply.github.com>
2019-05-17 20:40:27 +0800

    Create 4.3.3.6.c

---
+++ 前锋_C语言_/第四章/4.3.3.6/4.3.3.6.c
@@ -0,0 +1,26 @@
+/*
+ * liu韵姿 择偶的标准:
+ * 01. 男性
+ * 02. 年龄大于或等于 22 周岁
+ * 03. 并且小于 40 周岁
+ */
+
+#include <stdio.h>
+
+int main(void){
+  printf("输入一个性别 , 只能输入 m(男生) or f(女生)");
+  char buf[256];
+  fgets (buf , sizeof(buf) , stdin);
+  char gender;
+  sscanf(buf , "%c" , &gender);
+  printf("How old are you?\n");
+  fgets (buf , sizeof(buf) , stdin);
+  short age;
+  sscanf(buf , "%hd" , &age);
+  if (gender == 'm' && (age >= 22) && (age < 40)){
+    printf("明天我要嫁给你啦!\n");
+  } else {
+    printf("你是不是该安静地走开!\n");
+  }
+  return 0;
+}
