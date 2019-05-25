02aba3b1fc9d0016420bb8eba1b2cb76c5a3ecd0
CLAY <48152371+mineCLAY@users.noreply.github.com>
2019-05-25 15:15:36 +0800

    Create 第五题.c

---
+++ 前锋_C语言_/第四章/综合实际应用编程题/第五题.c
@@ -0,0 +1,22 @@
+/*
+ * Complete_Square_For_mula
+ * 编程 , 从键盘输入 a 和 b 的值
+ * 验证:
+ * (a + b)平方 = a平方 + 2ab + b平方
+ */
+ 
+#include <stdio.h>
+
+int main(void){
+    int a , b;
+    printf("输入 a 和 b:\n");
+    scanf("%d %d" , &a , &b);
+    if((a + b) * (a + b) == (a * a + 2 * a * b + b * b)){
+        printf("(a + b) * (a + b) = %d\n" , (a + b) * (a + b));
+        printf("(a * a + 2 * a * b + b * b) = %d\n" , (a * a + 2 * a * b + b * b);
+    } else {
+        printf("(a + b) * (a + b) != (a * a + 2 * a * b + b * b)");
+        printf("再检查一下程序 , 一定是搞错了.\n");
+    }
+    return 0;
+}
