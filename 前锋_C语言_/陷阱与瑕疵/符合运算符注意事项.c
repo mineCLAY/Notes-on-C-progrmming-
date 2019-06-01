d816038670ed8bbc09d61b9a9b1eb5dfd814c578
CLAY <48152371+mineCLAY@users.noreply.github.com>
2019-06-01 17:03:41 +0800

    Update 符合运算符注意事项.c

--- 前锋_C语言_/陷阱与瑕疵/符合运算符注意事项.c
+++ 前锋_C语言_/陷阱与瑕疵/符合运算符注意事项.c
@@ -5,12 +5,12 @@
 int main(void){
   int a = 2 , b = 3 , c = 4 , d = 5 , e = 6;
   d = (a = b + c) * e; // 可读性低
-  printf("a = %d , b = %d , c = %d , d  = %d , e = %d\n" ,
+  printf("a = %d , b = %d , c = %d , d = %d , e = %d\n" ,
           a , b , c , d , e);
 
   a = b + c; // 可读性
   d = a * e; // 高
-  printf("a = %d , b = %d , c = %d , d = %d , e = 6" ,
+  printf("a = %d , b = %d , c = %d , d = %d , e = 6\n" ,
           a , b , c , d , e);
 
   return 0;
