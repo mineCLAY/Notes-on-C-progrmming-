eb50d7b49d6cf6dc4af2cf7f758b6fa3a87783aa
CLAY <48152371+mineCLAY@users.noreply.github.com>
2019-06-01 17:26:56 +0800

    Create 小复习一下头文件.c

---
+++ 前锋_C语言_/陷阱与瑕疵/小复习一下头文件.c
@@ -0,0 +1,7 @@
+01. stdio.h 里定义了输入 (scanf) 和 输出 (printf) 等函数
+
+02. math.h 里定义了各种数学函数 , 如 double sqrt(double x) , double log(double x) , double sin(double x) 等等
+
+03. string.h 里定义各种字符串处理的函数 , 如 strcmp()、strcpy()等等
+
+04. stdlib.h 里定义了 EXIT_SUCCESS 和 EXIT_FAILURE 符号 , 表示成功与失败
