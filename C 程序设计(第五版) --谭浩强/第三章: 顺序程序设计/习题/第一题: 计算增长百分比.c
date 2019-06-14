5ed5c16db38f91b426501395ee996cc6f9df88ac
CLAY <48152371+mineCLAY@users.noreply.github.com>
2019-06-14 18:32:34 +0800

    Create 第一题: 计算增长百分比.c

---
+++ C 程序设计(第五版) --谭浩强/第三章: 顺序程序设计/习题/第一题: 计算增长百分比.c
@@ -0,0 +1,18 @@
+/*
+ * exercise
+ *
+ * 假如我国国民生产总值的年增长率为 7% 
+ * 计算 10 年后 我国国民生产总值与现在相比增长多少百分比
+ */
+
+#include <stdio.h>
+
+int main(void) {
+
+  double r = 0.07 , years , p , multiple;  // 定义　增长率　, 念书 , 倍数
+  p = (1 + 0.07) * 10;                     // 计算现在相比的倍数
+
+  printf("multiple = %lf\n" , p);          // 输出现在相比的倍数
+
+  return 0;
+}
