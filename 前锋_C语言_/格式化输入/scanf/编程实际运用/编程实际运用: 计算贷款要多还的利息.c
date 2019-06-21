4bffec1e3079a55b555046463d65f4218114d528
CLAY <48152371+mineCLAY@users.noreply.github.com>
2019-06-21 18:58:04 +0800

    Create 编程实际运用: 计算贷款要多还的利息.c

---
+++ 前锋_C语言_/格式化输入/scanf/编程实际运用/编程实际运用: 计算贷款要多还的利息.c
@@ -0,0 +1,29 @@
+/*
+ * 5.06 IPP_scanf.c
+ *
+ * 键盘输入:
+ * 贷款的金额、贷款年数、每月还款金额
+ * 计算贷款要多还的利息
+ */
+
+#include <stdio.h>
+
+int main(void) {
+
+  printf("输入贷款金额:\n");
+  int loan;
+  scanf("%d" , &loan);
+
+  printf("输入每月还款金额:\n");
+  int payment;
+  scanf("%d" , &payment);
+
+  printf("输入还款年数:\n");
+  int year;
+  scanf("%d" , &year);
+
+  int rate = payment * 12 * year - loan; // 计算利息
+  printf("贷款需要多还: %d\n" , rate);     // 输出结果: 多还的钱
+
+  return 0;
+}
