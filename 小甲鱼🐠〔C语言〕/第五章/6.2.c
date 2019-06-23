ead8922db986159b2e767ae3964e66d2199a702a
CLAY <48152371+mineCLAY@users.noreply.github.com>
2019-06-24 05:06:53 +0800

    Create 6.2.c

---
+++ 小甲鱼🐠〔C语言〕/第五章/6.2.c
@@ -0,0 +1,14 @@
+//用 while 语句构成循环求 1 + 2 + 3 + ...... + 100
+
+#include <stdio.h>
+
+int main( ){
+  int i , sum = 0;
+  i = 1;
+  while (i <= 100){
+    sum = sum + i;
+    i++;
+  }
+  printf("%d\n" , sum);
+  return 0;
+}
