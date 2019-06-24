251e5ecbd4eaf8cd6f2a047574fa65ccfc696b4e
CLAY <48152371+mineCLAY@users.noreply.github.com>
2019-06-24 14:05:30 +0800

    Create 第一题: 将用户名与密码存到文件中.c

---
+++ 前锋_C语言_/从流中得到一行字符/fgets/课后问题/第一题: 将用户名与密码存到文件中.c
@@ -0,0 +1,20 @@
+/*
+ * 键盘输入你的用户名、密码、确认密码
+ * 如果两次密码不一样就提示错误
+ *
+ * 如果正确就把用户名、密码
+ * 存入
+ * 以 "用户名" 命名的文件中
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+
+int mian(void) {
+
+  
+
+  return 0;
+}
