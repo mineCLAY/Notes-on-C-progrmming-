2baa4fb595fde2c65893b5f4ebca604c46950ae6
CLAY <48152371+mineCLAY@users.noreply.github.com>
2019-06-27 08:56:02 +0800

    Create 过滤空格[失败].c

---
+++ 前锋_C语言_/重构/缓冲区溢出/过滤空格[失败].c
@@ -0,0 +1,29 @@
+/*
+ * 过滤空格
+ *
+ * 11.03 Buffer_Residue_Original.c 
+ */
+
+#include <stdio.h>
+
+int main(void) {
+
+  char vote[6];
+
+  // 叫用户输入一行字符串
+  puts("Enter string:");
+  // 开始循环 赋值
+  for (int i = 0; i <5; ++i) {
+    scanf("%c" , &vote[i]);
+  }
+
+  // 开始循环 输出
+  for (int i = 0; i < 5; ++i) {
+    printf("%c " , vote[i]);
+  }
+
+  // 输换行出
+  putchar('\n');
+
+  return 0;
+}
