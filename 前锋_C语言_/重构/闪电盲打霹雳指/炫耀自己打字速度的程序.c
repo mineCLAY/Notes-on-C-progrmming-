20f29f715d535e188f730e96411afdd3e5f79efa
CLAY <48152371+mineCLAY@users.noreply.github.com>
2019-06-28 19:34:52 +0800

    Update 炫耀自己打字速度的程序.c

--- 前锋_C语言_/重构/闪电盲打霹雳指/炫耀自己打字速度的程序.c
+++ 前锋_C语言_/重构/闪电盲打霹雳指/炫耀自己打字速度的程序.c
@@ -3,38 +3,42 @@
  *
  * Hacker Coding
  */
-
-#include <conio.h>
-#include <curses.h>
-#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <errno.h>
+#include <curses.h>
+#include <string.h>
 
 int main(int argc , char *argv[]) {
 
   if (argc < 2) {
     fprintf(stderr , "Uage: %s <filename>\n" , argv[0]);
+    // 关闭程序
     exit(EXIT_FAILURE);
   }
 
   FILE *fp;
-  if (NULL == (fp == fopen(argv[1] , "r"))) {
+  if (NULL == (fp = fopen(argv[1] , "r"))) {
     fprintf(stderr , "fopen: %s\n" , strerror(errno));
+    // 关闭程序
     exit(EXIT_FAILURE);
   }
 
+  // 设置窗口
   initscr();
+  // 开始循环读取用户输入的数据
   do {
     int c = fgetc(fp);
-    if (fepof(fp)) {
+    if (feof(fp)) {
       break;
     }
     getch();
     printf("%c" , c);
   } while(1);
+  // 关闭窗口
   endwin();
 
+  //  关闭流 fp 防止内存泄漏
   fclose(fp);
 
   return 0;
