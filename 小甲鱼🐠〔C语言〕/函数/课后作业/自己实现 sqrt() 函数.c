11c03cc663627419abd142f8e84420d90dfbc33f
CLAY <48152371+mineCLAY@users.noreply.github.com>
2019-07-02 07:24:46 +0000

    Update 自己实现 sqrt() 函数.c

--- 小甲鱼🐠〔C语言〕/函数/课后作业/自己实现 sqrt() 函数.c
+++ 小甲鱼🐠〔C语言〕/函数/课后作业/自己实现 sqrt() 函数.c
@@ -13,18 +13,43 @@
 int main(void) {
 
   // 声明 sqrt() 函数
-  int sqrt(int x , int y); 
+  int sqrt(int x);
+  // 定义所需变量
+  int a , square;
+
+  // 叫用户输入 1 个值
+  puts("Enter a number:");
+  // 将用户输入的值赋给变量 a
+  scanf("%d" , &a);
+
+  // 将函数返回值赋给变量 square 
+  square = sqrt(a);
+  // 输出结果
+  printf("The square root of %d is %d" ,
+          a , square);
 
   return 0;
 }
 
 // 定义 sqrt() 函数
-int sqrt(int x , int y) {
+int sqrt(int x) {
 
   // 声明存储结果的变量
-  int square;
+  int square , i;
 
-  
+  // 开始循环找出平方根
+  for (i = 1; i <= x; i++) {
+    // 判断是否求出平方根
+    if (i == x / i) {
+      // 如果求出.则退出循环
+      break;
+    }
+  }
 
+  // 将求出的值赋给变量 square
+  square = i;
+
+
+  // 返回求出的值
   return square;
 }
