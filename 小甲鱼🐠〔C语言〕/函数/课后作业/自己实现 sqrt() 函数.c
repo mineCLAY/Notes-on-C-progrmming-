a310b004498db4db66a2c2c6ecf2334c767d471a
CLAY <48152371+mineCLAY@users.noreply.github.com>
2019-07-02 01:53:30 +0000

    Create 自己实现 sqrt() 函数.c

---
+++ 小甲鱼🐠〔C语言〕/函数/课后作业/自己实现 sqrt() 函数.c
@@ -0,0 +1,18 @@
+/*
+ * 猜想下 sqrt() 函数的原理
+ * 并尝试编程(暂时只要求整型数据)
+ *
+ * sqrt() 函数:
+ * 存放于 math.h 头文件中
+ * 作用:
+ * 计算平方根
+ */
+
+#include <stdio.h>
+
+int main(void) {
+
+  int sqrt(int x , int y);
+
+  return 0;
+}
