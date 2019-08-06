a373e0ee2b12640669096df205dd24765e2ab624
CLAY <48152371+mineCLAY@users.noreply.github.com>
2019-08-06 14:30:23 +0800

    Create 打印英雄属性.cpp

---
+++ Note-C++/C++基础/数据类型与运算符/编译规范/打印英雄属性.cpp
@@ -0,0 +1,18 @@
+/*
+ * 打印英雄属性
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+
+  // 输出对象
+  cout << "英雄名称: 阿轲" << endl;
+  cout << "生命值: 4500\t生命回复: 3\n"
+       << "伤害: 56\t攻击距离: 350\n"
+       << "护甲: 38( + 66)\t魔抗: 30( + 20)" << endl;
+
+  return 0;
+}
