550ae28130d66f72b4255d28574a827874ea01b4
CLAY <48152371+mineCLAY@users.noreply.github.com>
2019-08-23 04:08:44 +0000

    Create 依然敲三遍代码.cpp

---
+++ Note-C++/C++基础/数据类型与运算符/作业/依然敲三遍代码.cpp
@@ -0,0 +1,61 @@
+/*
+ * 敲三遍代码
+ * (我直接 copy)
+ */
+
+#include <iostream>
+
+using namespacestd;
+
+int main(void) {
+
+  // 打印
+  cout << "int      整型" << endl;
+  cout << "short    短整型" << endl;
+  cout << "bool     布尔型" << endl;
+  cout << "char     字符类型" << endl;
+  cout << "float    单精度浮点型" << endl;
+  cout << "double   双精度浮点型" << endl;
+
+  return 0;
+}
+
+// 开关 0 不执行
+#if 0
+
+#include <iostream>
+
+using namespacestd;
+
+int main(void) {
+
+  // 打印
+  cout << "int      整型" << endl;
+  cout << "short    短整型" << endl;
+  cout << "bool     布尔型" << endl;
+  cout << "char     字符类型" << endl;
+  cout << "float    单精度浮点型" << endl;
+  cout << "double   双精度浮点型" << endl;
+
+  return 0;
+}
+
+#include <iostream>
+
+using namespacestd;
+
+int main(void) {
+
+  // 打印
+  cout << "int      整型" << endl;
+  cout << "short    短整型" << endl;
+  cout << "bool     布尔型" << endl;
+  cout << "char     字符类型" << endl;
+  cout << "float    单精度浮点型" << endl;
+  cout << "double   双精度浮点型" << endl;
+
+  return 0;
+}
+
+// 结束预处理 if 0
+#endif
