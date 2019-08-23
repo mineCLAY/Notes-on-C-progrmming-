35f11242bcafb9c15321453c17513452a7b42c31
CLAY <48152371+mineCLAY@users.noreply.github.com>
2019-08-23 04:11:05 +0000

    Create 随意打印.cpp

---
+++ Note-C++/C++基础/数据类型与运算符/作业/随意打印.cpp
@@ -0,0 +1,13 @@
+/*
+ * 随意打印
+ */
+
+#include <iostream>
+
+int main(void) {
+
+  // 打印
+  cout << "I'm GOD\bd\bd\bd\bd\bd\bd\bd\bd\bd\bd\bd\bd\bd\bd\bd\bd\bd\bd\bd\bd\bd\bd\bd\bd\bd\bd\bd\bd\bd\bd\bD" << endl;
+
+  return 0;
+}
