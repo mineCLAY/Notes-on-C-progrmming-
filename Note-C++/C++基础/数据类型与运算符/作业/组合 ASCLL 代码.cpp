edd67e0230f145b2b846485abb3692effb033e80
CLAY <48152371+mineCLAY@users.noreply.github.com>
2019-08-23 07:11:42 +0000

    Create 组合 ASCLL 代码.cpp

---
+++ Note-C++/C++基础/数据类型与运算符/作业/组合 ASCLL 代码.cpp
@@ -0,0 +1,22 @@
+/*
+ * 将下面的数字按顺序转换成
+ * 相应的ASCII码
+ * 打印出相应的ASCII码
+ * 对应的字母并进行组合
+ * 数字如下:
+ * 97、110、103、101、108、97、66、97、98、121
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+
+  // 打印
+  cout << (char)97 << (char)110 << (char)103 << (char)101
+       << (char)108 << (char)97 << (char)98 << (char)97
+       << (char)98 << (char)121 << endl;
+
+  return 0;
+}
