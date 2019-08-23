c555a075a0a8aa37644ee57acc2c85ae30fa991b
CLAY <48152371+mineCLAY@users.noreply.github.com>
2019-08-23 03:51:36 +0000

    Create 敲三遍代码.cpp

---
+++ Note-C++/C++基础/数据类型与运算符/敲三遍代码.cpp
@@ -0,0 +1,69 @@
+/*
+ * Operation
+ *
+ * 敲三遍代码
+ * (我直接 copy) 
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+
+  // 打印
+  cout << "#                       预处理" << endl;
+  cout << "include                 包含" << endl;
+  cout << "cout                    输出" << endl;
+  cout << "<<                      插入运算符" << endl;
+  cout << "endl                    结束输出" << endl;
+  cout << "iostream                头文件" << endl;
+  cout << "main                    主要的" << endl;
+  cout << "using namespace std     使用命名空间 std" << endl;
+
+  return 0;
+}
+
+// 开关 0 (不执行)
+#if 0
+
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+
+  // 打印
+  cout << "#                       预处理" << endl;
+  cout << "include                 包含" << endl;
+  cout << "cout                    输出" << endl;
+  cout << "<<                      插入运算符" << endl;
+  cout << "endl                    结束输出" << endl;
+  cout << "iostream                头文件" << endl;
+  cout << "main                    主要的" << endl;
+  cout << "using namespace std     使用命名空间 std" << endl;
+
+  return 0;
+}
+
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+
+  // 打印
+  cout << "#                       预处理" << endl;
+  cout << "include                 包含" << endl;
+  cout << "cout                    输出" << endl;
+  cout << "<<                      插入运算符" << endl;
+  cout << "endl                    结束输出" << endl;
+  cout << "iostream                头文件" << endl;
+  cout << "main                    主要的" << endl;
+  cout << "using namespace std     使用命名空间 std" << endl;
+
+  return 0;
+}
+
+// 结束预处理 if 0
+#endif
