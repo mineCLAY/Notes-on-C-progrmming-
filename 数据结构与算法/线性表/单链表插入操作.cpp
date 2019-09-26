960f2f3f9f8051c2a84e77a7282e9ada43de9caf
CLAY <48152371+mineCLAY@users.noreply.github.com>
2019-09-27 06:15:53 +0800

    Update 单链表插入操作.cpp

--- 数据结构与算法/线性表/单链表插入操作.cpp
+++ 数据结构与算法/线性表/单链表插入操作.cpp
@@ -2,7 +2,7 @@
  * 单链表插入操作
  */
 
-Status ListInsert(LinkList *L , EmlemType temp) {
+Status ListInsert(LinkList *L , int po , ElemType temp) {
 
   /*
    * 定义存储
@@ -14,21 +14,20 @@ Status ListInsert(LinkList *L , EmlemType temp) {
    * 结点的临时变量
    */
   LinkList *t;
-
-  // 创建链表
-  List list;
+  // 定义指向链表指针变量
+  LinkList *p;
 
   /*
    * 定义线性表类型
    * 指针变量
-   * 让指针 p
+   * 让指针 position
    * 指向 list 线性表
    * 的头指针
    */
-  List *p = list;
+  p = *L;
 
   // 开始循环查找
-  for (position = 1; position <= temp; position++) {
+  for (position = 1; p && (position < po); position++) {
 
     // 开始递进结点
     p = p -> next;
@@ -39,18 +38,24 @@ Status ListInsert(LinkList *L , EmlemType temp) {
    * 判断是否没有
    * 找到插入位置
    */
-  if (position > list.length) {
+  if (!p || position > po) {
 
     // 返回错误
     return ERROR;
 
   }
 
+  /*
+   * 给临时指针
+   * 变量分配内存
+   */
+  t = (LinkList) malloc (sizeof(Node));
+  // 将数据赋值给成员变量
+  t -> data = temp;
+
   // 开始插入数据
-  t = p -> next;
-  p -> next = temp;
-  p = p -> next;
-  p -> next  = t;
+  t -> next = p -> next;
+  p -> next = t;
 
   return TRUE;
 }
